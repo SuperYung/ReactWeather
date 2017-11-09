3f2f02420a069a0ef1f50dc01c07df1e9ccbeedc
Yung-Feng Chou (r626884) <yung-feng.chou@cambiahealth.com>
2017-11-08 23:03:05 -0800

    init setup

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,22 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=fd1ea81d32a2ac5e922484b22956134b&units=imperial';
+
+// fd1ea81d32a2ac5e922484b22956134b
+
+module.exports = {
+  getTemp: function (location) {
+    var encodeLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodeLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    })
+  }
+}
